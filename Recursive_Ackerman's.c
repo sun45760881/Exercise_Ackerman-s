73580329ae28f8601fab36ec8763a82f1d73c9ea
sun45760881 <sun45760881@gmail.com>
2016-04-05 15:35:05 +0800

    Create Recursive_Ackerman's.c

---
+++ Recursive_Ackerman's.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+int main(void) {
+	int n, m;
+	printf("A(m,n)\nm=");
+	scanf("%d",&m);
+	printf("n=");
+	scanf("%d",&n);
+	printf("A(%d,%d)=%d\n",m,n,A(m,n));
+}
+
+
+int A(int m, int n) 
+{
+	if (m == 0)
+	{
+		return n + 1;
+	}
+	else if (n == 0)
+	{
+		return A(m - 1, 1);
+	}
+	else
+	{
+		return A(m - 1, A(m, n - 1));
+	}
+}
+
+
+
+/*
+
+A(m,n)= n+1 if(m==0)
+		A(m-1,1) else if(n==0)
+		A(m-1,A(m,n-1)) else
+
+
+*/
